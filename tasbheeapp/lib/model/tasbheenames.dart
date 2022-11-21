d9b7fe2a51809d67091456d69332383090fc42ce
Usrabajwa1 <usrabajwa0@gmail.com>
2022-11-21 00:24:24 -0800

    project

--- tasbheeapp/lib/model/tasbheenames.dart
+++ tasbheeapp/lib/model/tasbheenames.dart
@@ -17,7 +17,7 @@ class TasbheeNames extends StatelessWidget {
   String? get dateCreate=> _dateCreate;
   int ? get id=> _id;
 
-  Map<String , dynamic> toMap(){
+  Map<String , dynamic> toMap() {
     var map = new Map<String, dynamic>();
     map["itemName"] = _itemName;
     map["dateCreate"] = _dateCreate;
@@ -25,10 +25,50 @@ class TasbheeNames extends StatelessWidget {
     if(_id !=null){
       map["id"]= _id;
     }
+    return map;
+  }
+
+  TasbheeNames.fromMap(Map<String, dynamic>map){
+    this._itemName = map["itemName"];
+    this._dateCreate =  map["dateCreate"];
+    this._id= map["id"];
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      margin: const EdgeInsets.all(8.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                _itemName!,
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16.9
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 5.0),
+                child: Text(
+                  "Created on : $_dateCreate",
+                  style: TextStyle(
+                      color: Colors.black45,
+                      fontSize: 12.5,
+                      fontStyle: FontStyle.italic),
+
+                ),
+              ),
+
+            ],
+          )
+
+        ],
+      ),
+    );
   }
 }
--- tasbheeapp/lib/ui/tasbheescreen.dart
+++ tasbheeapp/lib/ui/tasbheescreen.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:tasbheeapp/model/tasbheenames.dart';
+import 'package:tasbheeapp/util/database.dart';
 class TasbheeScreen extends StatefulWidget {
-  const TasbheeScreen({Key? key}) : super(key: key);
 
   @override
   State<TasbheeScreen> createState() => _TasbheeScreenState();
 }
 
 class _TasbheeScreenState extends State<TasbheeScreen> {
+  final TextEditingController _textEditingController = TextEditingController();
+  var db = new DatabaseHelper();
+  final List<TasbheeNames> _itemList = <TasbheeNames>[];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,6 +27,51 @@ class _TasbheeScreenState extends State<TasbheeScreen> {
     );
   }
 
-  void _ShowdDialogForm() {
+  void _ShowdDialogForm() async {
+    var alert = AlertDialog(
+      content: Row(
+        children: [
+          Expanded(child: TextField(
+            controller: _textEditingController,
+            autofocus: true,
+            decoration: InputDecoration(
+                labelText: "Item",
+                hintText: "eg. Don't ",
+                icon: Icon(Icons.note_add)
+            ),
+          ))
+
+        ],
+      )
+      actions: <Widget>[
+        Textbutton(
+     onPressed: (){
+       _handleSubmitted(_textEditingController.text);
+       _textEditingController.clear();
+       Navigator.pop(context);
+
+    },
+    )
+        ]
+    ),
+    showDialog(
+    context: context,
+        builder: (_)
+    {
+        return alert;
+    }
+    );
+  }
+
+  void _handleSubmitted(String text) async {
+    _textEditingController.clear();
+    TasbheeNames tasbheenames = TasbheeNames(text, DateTime.now().toIso8601String());
+    int savedItem = await db.saveItem(tasbheenames);
+    TasbheeNames addedItem = await db.getItem(savedItem);
+    setState(() {
+      _itemList.insert(0,addedItem);
+    });
+    print("Item saved id : $savedItem")
+    ;
   }
 }
